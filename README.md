20e6b06e4147801218c21481b00155d38065794c
MackRoe <elaine.music@students.makeschool.com>
2021-02-25 18:22:01 -0500

    Complete second tutorial

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Testing In Go
 
-Follows tutorial at [Tutorial Edge's 'An Introduction to Testing in Go'](https://tutorialedge.net/golang/intro-testing-in-go/)
+Follows tutorials at [Tutorial Edge's 'An Introduction to Testing in Go'](https://tutorialedge.net/golang/intro-testing-in-go/) and ['An Introduction to Benchmarking Your Go Programs'](https://tutorialedge.net/golang/benchmarking-your-go-programs/)
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -28,3 +29,28 @@ func TestTableCalculate(t *testing.T) {
 		}
 	}
 }
+func BenchmarkCalculate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Calculate(2)
+	}
+}
+func TestCalculate2(t *testing.T) {
+	fmt.Println("Test Calculate")
+	expected := 4
+	result := Calculate(2)
+	if expected != result {
+		t.Error("Failed")
+	}
+}
+func TestOther(t *testing.T) {
+	fmt.Println("Testing something else")
+	fmt.Println("This shouldn't run with -run=calc")
+}
+func benchmarkCalculate(input int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Calculate(input)
+	}
+}
+func BenchmarkCalculate100(b *testing.B)         { benchmarkCalculate(100, b) }
+func BenchmarkCalculateNegative100(b *testing.B) { benchmarkCalculate(-100, b) }
+func BenchmarkCalculateNegative1(b *testing.B)   { benchmarkCalculate(-1, b) }
